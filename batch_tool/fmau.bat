0bc79b76001ea633bb69bd8b3ae0e3c362b663ad
Simouie <simon-louie@outlook.com>
2022-04-28 14:42:15 -0700

    Stop tracking fmau.bat

--- batch_tool/fmau.bat
+++
@@ -1,2 +0,0 @@
-tool fp-model-animations-uncompressed dw3\r3ach\objects\weapons\pistol\needler\animations\fp_elite objects\characters\dervish\fp dw3\r3ach\objects\weapons\pistol\needler\needler_fp
-tool fp-model-animations-uncompressed dw3\r3ach\objects\weapons\pistol\needler\animations\fp_spartan objects\characters\masterchief\fp dw3\r3ach\objects\weapons\pistol\needler\needler_fp
